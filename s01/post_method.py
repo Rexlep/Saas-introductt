b7844062d438fdd2981bfc742a39ab13e44aca2b
Rexlep <141561659+Rexlep@users.noreply.github.com>
2026-05-31 11:02:02 +0330

    Coded

--- s01/post_method.py
+++ s01/post_method.py
@@ -9,3 +9,13 @@ def register(name: str):
     return {
         "message": f"Welcome {name}"
     }
+
+
+@app.get("/product")
+def get_products():
+    return ['Laptop',  'Mouse', 'Keyboard']
+
+
+@app.get("/products")
+def create_product():
+    return {"message": "Product Created"}
