dce618b1e609c27d51d1a9453806725e06c48a76
Rexlep <141561659+Rexlep@users.noreply.github.com>
2026-05-31 11:28:31 +0330

    Coded

--- s01/task_manager.py
+++ s01/task_manager.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 tasks = []
+
+
+class Task(BaseModel):
+    title: str
 
 
 @app.get("/tasks")
@@ -11,8 +16,8 @@ def get_tasks():
 
 
 @app.post("/tasks")
-def create_task():
-    tasks.append("Learn FastAPI")
+def create_task(task: Task):
+    tasks.append(task.title)
 
     return {
         "message": "Task Added"
